691e96148937467076b74cf2d441b2a5cdceb2c0
Raul Barriga <barrigaraulaug@gmail.com>
2020-09-24 15:20:10 -0700

    Updates

---
+++ precache-manifest.f0fbddab3ce5ca3fc0fbae76d13aae4b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0352332c490f92373f4c2b1f2ef36a30",
+    "url": "/tekashi69-artist-website/index.html"
+  },
+  {
+    "revision": "0a5584df446932075f05",
+    "url": "/tekashi69-artist-website/static/css/main.d367674d.chunk.css"
+  },
+  {
+    "revision": "51948eb9837ca69207fc",
+    "url": "/tekashi69-artist-website/static/js/2.db7be3a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tekashi69-artist-website/static/js/2.db7be3a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a5584df446932075f05",
+    "url": "/tekashi69-artist-website/static/js/main.0284bc70.chunk.js"
+  },
+  {
+    "revision": "507ec38acae9134b906f",
+    "url": "/tekashi69-artist-website/static/js/runtime-main.5cfe03b0.js"
+  },
+  {
+    "revision": "4a670eb767163574890491f97cf488a0",
+    "url": "/tekashi69-artist-website/static/media/album.4a670eb7.jpg"
+  },
+  {
+    "revision": "a4a29ab81b15ca6168cf74e7cd492a6f",
+    "url": "/tekashi69-artist-website/static/media/group.a4a29ab8.png"
+  },
+  {
+    "revision": "451d7fc5aece9130f0d4914ffa52793a",
+    "url": "/tekashi69-artist-website/static/media/logo.451d7fc5.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tekashi69-artist-website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0116239985ed4148a1ffd5a0ad45374f",
+    "url": "/tekashi69-artist-website/static/media/tekashi.01162399.png"
+  }
+]);
